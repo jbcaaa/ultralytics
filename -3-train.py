e5571f00333fa04a1f1f5f86fe42a2f087fd7b6d
jbcaaa <2712825619@qq.com>
2024-11-26 18:34:16 +0800

    commit

--- -3-train.py
+++ -3-train.py
@@ -1,13 +1,12 @@
 from ultralytics import YOLO
 
 if __name__ == '__main__':
-    # model.load('yolo11n.pt') # 加载预训练权重,改进或者做对比实验时候不建议打开，因为用预训练模型整体精度没有很明显的提升
     model = YOLO(model=r'ultralytics\cfg\models\11\yolo11.yaml')
+    model.load('best.pt') # 加载预训练权重,改进或者做对比实验时候不建议打开，因为用预训练模型整体精度没有很明显的提升
     model.train(data=r'data.yaml',
                 imgsz=1280,
                 epochs=1000,
                 batch=-1,
-                workers=2,
                 device='0',
                 optimizer='SGD',
                 close_mosaic=10,
